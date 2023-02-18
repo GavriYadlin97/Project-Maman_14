142073ba61cb34ceaa5095bb89ac5383ded3aa90
GavriYadlin97 <gavriel131@gmail.com>
2023-02-18 17:27:57 +0200

    רשמתי הערה במיין

--- main.c
+++ main.c
@@ -4,10 +4,14 @@
 int main(int argc, char *argv[]) {
     printf("Hello, World!\n");
     char* line = (char*) malloc(sizeof (char )*LINE_MAX_LENGTH);
-    line= "hello,world!";
-    char** word= "";
+    //ברגע ששמת לו משפט כמו כאן, זה כבר string literal כלומר אסור לתוכנית לשנות אותו, צריך לעשות את זה עם קליטה של משתנה
+    //line= "hello,world!";
+    fgets(line,LINE_MAX_LENGTH,stdin);
+    //חבל להתעסק עם dereferance אחר כך, כוכבית אחת זה מספיק (לשלוח לפונקצייה עם &) ולא לאתחל אותו לכלום
+    // char ** word="";
+    char* word;
 
-    getToken(&line,word,",");
+    getToken(&line,&word,",");
 
     char* fileName= argv[1];
     FILE* stream;
